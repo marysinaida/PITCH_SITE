9ec8592e347101325fc4ce394c8c46da92fca956
marysinaida <marydorcassinaida54@gmail.com>
2019-11-26 01:17:06 +0300

    add a full-colon

--- pitchblog.py
+++ pitchblog.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask,render_template,url_for,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
@@ -9,7 +10,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
@@ -17,6 +18,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 
 posts = [
